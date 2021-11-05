460a2f49c41335af375366f94b08e8b2e842f04a
Bruno Thiao Layel <bruno.thiaolayel@gmail.com>
2021-11-05 13:22:06 +0100

    same

--- demo/src/main/java/com/example/demo/rest/RestCalculatrice.java
+++ demo/src/main/java/com/example/demo/rest/RestCalculatrice.java
@@ -3,12 +3,20 @@ package com.example.demo.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.calculatrice.CalculatriceService;
 
+import io.swagger.annotations.Api;
+
+
+@Controller
+@RequestMapping("/calculatrice")
+@Api(value = "Calculatrice API")
 public class RestCalculatrice {
 	
 	@Autowired
